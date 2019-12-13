8afedbab6281592c94e6a87b24a31ceeed8f4b84
Chris Comora <cgcomora@ncsu.edu>
2019-12-13 18:13:37 -0500

    moved helpers.R file into server.R

--- server.R
+++ server.R
@@ -7,6 +7,44 @@
 #    http://shiny.rstudio.com/
 
 shinyServer(function(input, output,session){
+  
+  crimes <- read.csv("st558projectfinal/hate_crimes.csv")
+  # initialize variables for each region of the U.S. 
+  northeast <- c("Maine","New Hampshire","Vermont",
+                 "Massachusetts","Rhode Island",
+                 "Connecticut","New York","New Jersey",
+                 "Pennsylvania")
+  midwest <- c("Ohio","Michigan","Indiana","Illinois",
+               "Wisconsin","Minnesota","Iowa","Missouri",
+               "North Dakota", "South Dakota","Nebraska",
+               "Kansas")
+  south <- c("Texas","Oklahoma","Arkansas","Louisiana",
+             "Mississippi","Tennesee","Alabama","Georgia",
+             "Florida","West Virginia", "Kentucky",
+             "Delaware","Maryland","Virginia","North Carolina",
+             "South Carolina","District of Columbia")
+  west <- c("Washington","Idaho","Montana","Wyoming",
+            "Oregon","California","Nevada","Utah",
+            "Arizona","Colorado","New Mexico",
+            "Alaska","Hawaii")
+  region <- vector()
+  for (i in 1:length(crimes$state)){
+    if(is.element(crimes$state[i], northeast)==TRUE){
+      region[i]= "northeast"}
+    else if(is.element(crimes$state[i], midwest)==TRUE){
+      region[i]= "midwest"}
+    else if(is.element(crimes$state[i], west)==TRUE){
+      region[i] = "west"}
+    else {region[i] = "south"}
+  }   
+  
+  #define new column in data set with region assigned to state
+  crimes$region <- region
+  #create dataframe for unsupervised learning
+  df <- column_to_rownames(crimes, var = "state")
+  df <- na.omit(df)
+  df <- select(df, -12)
+  df_scale <- scale(df)
      output$introduction <- renderText({
         "This is my introduction"
     })
