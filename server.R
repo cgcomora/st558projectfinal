51d7b9f479df9dcd584f63dba7ce701bd13b7482
Chris Comora <cgcomora@ncsu.edu>
2019-12-13 19:36:48 -0500

    changed name of helpers.R to data.R removed source() and replaced with load()

--- server.R
+++ server.R
@@ -6,46 +6,11 @@
 #
 #    http://shiny.rstudio.com/
 
+load(data.R)
+
 shinyServer(function(input, output,session){
   
-  crimes <- read.csv("cgcomora/st558projectfinal/hate_crimes.csv")
-  # initialize variables for each region of the U.S. 
-  northeast <- c("Maine","New Hampshire","Vermont",
-                 "Massachusetts","Rhode Island",
-                 "Connecticut","New York","New Jersey",
-                 "Pennsylvania")
-  midwest <- c("Ohio","Michigan","Indiana","Illinois",
-               "Wisconsin","Minnesota","Iowa","Missouri",
-               "North Dakota", "South Dakota","Nebraska",
-               "Kansas")
-  south <- c("Texas","Oklahoma","Arkansas","Louisiana",
-             "Mississippi","Tennesee","Alabama","Georgia",
-             "Florida","West Virginia", "Kentucky",
-             "Delaware","Maryland","Virginia","North Carolina",
-             "South Carolina","District of Columbia")
-  west <- c("Washington","Idaho","Montana","Wyoming",
-            "Oregon","California","Nevada","Utah",
-            "Arizona","Colorado","New Mexico",
-            "Alaska","Hawaii")
-  region <- vector()
-  for (i in 1:length(crimes$state)){
-    if(is.element(crimes$state[i], northeast)==TRUE){
-      region[i]= "northeast"}
-    else if(is.element(crimes$state[i], midwest)==TRUE){
-      region[i]= "midwest"}
-    else if(is.element(crimes$state[i], west)==TRUE){
-      region[i] = "west"}
-    else {region[i] = "south"}
-  }   
-  
-  #define new column in data set with region assigned to state
-  crimes$region <- region
-  #create dataframe for unsupervised learning
-  df <- column_to_rownames(crimes, var = "state")
-  df <- na.omit(df)
-  df <- select(df, -12)
-  df_scale <- scale(df)
-     output$introduction <- renderText({
+       output$introduction <- renderText({
         "This is my introduction"
     })
      
@@ -170,7 +135,6 @@ shinyServer(function(input, output,session){
 
 })
 
-#run the application
-shinyApp(ui,server)
+
 
 
